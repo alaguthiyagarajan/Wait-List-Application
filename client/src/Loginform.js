4399a268eb5865115d402e9db3bbb7475c9bbf7e
alaguthiyagarajan <132125380+alaguthiyagarajan@users.noreply.github.com>
2023-09-21 21:37:12 +0530

    Loginform.js

--- client/src/Loginform.js
+++ client/src/Loginform.js
@@ -3,12 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 import "./loginform.css"
 import { useState } from "react";
 function LoginForm(){
-    let [name,setName]=useState("");
-    let [pass,setPass]=useState("");
-    let navigate=useNavigate();
+    let [name,setName] = useState("");
+    let [pass,setPass] = useState("");
+    let navigate = useNavigate();
 
-    let login=()=>{
-        if(name.trim()=="admin" && pass.trim()=="admin"){
+    let login = () => {
+        if(name.trim() == "admin" && pass.trim() == "admin"){
             alert("login success");
             navigate("/admin");
         }
@@ -17,11 +17,11 @@ function LoginForm(){
         }
     };
 return (
-    <div className="container-fluid w-100 vh-100 justify-content-center align-items-center d-flex">
-    <div className="cover " >
-        <h1 className="" style = {{ fontSize : "4rem" }}> LOGIN </h1>
-        <input type = "text" style = {{ fontSize : "1rem", padding : "15px" }} placeholder = " USERNAME " value={name} onChange={(e)=>{setName(e.target.value)}} id="name"/>
-        <input type = "password" style = {{ fontSize : "1rem", padding : "15px" }} placeholder = "PASSWORD"id="password" value={pass} onChange={(e)=>{setPass(e.target.value)}} />
+    <div className = "container-fluid w-100 vh-100 justify-content-center align-items-center d-flex">
+    <div className = "cover " >
+        <h1 className = "" style = {{ fontSize : "4rem" }}> LOGIN </h1>
+        <input type = "text" style = {{ fontSize : "1rem", padding : "15px" }} placeholder = " USERNAME " value = {name} onChange = {(e) => {setName(e.target.value)}} id = "name"/>
+        <input type = "password" style = {{ fontSize : "1rem", padding : "15px" }} placeholder = "PASSWORD" id = "password" value = {pass} onChange = {(e) => {setPass(e.target.value)}} />
         <button className = "login-btn text-dark"  onClick={login}>Login</button>
     </div>
 
