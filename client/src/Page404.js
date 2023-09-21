da50126bdb5e3887c5335b341444ad64935be3f3
alaguthiyagarajan <132125380+alaguthiyagarajan@users.noreply.github.com>
2023-09-21 22:02:15 +0530

    Page404.js

--- client/src/Page404.js
+++ client/src/Page404.js
@@ -1,5 +1,5 @@
 function Page404 () {
-
+     //If any Error occured it shows 404 page url
     return(
         <div className = "container-fluid bg-primary vh-100 d-flex vw-100 justify-centent-center align-items-center">
             <h3 className = " fw-bold text-danger text-center  w-100" style = {{textShadow:"0px 0px 20px white", fontSize:"4rem"}}>404 Page URL</h3>
