674a548eb572aaee156f77cf6917d22f49431fa5
alaguthiyagarajan <132125380+alaguthiyagarajan@users.noreply.github.com>
2023-09-21 22:17:04 +0530

    mailset.js

--- server/mailset.js
+++ server/mailset.js
@@ -1,5 +1,5 @@
 let nodemailer = require('nodemailer');
-
+// In this process is Set Mail Id and set Temporarory Password
 let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
