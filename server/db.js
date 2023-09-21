26dd504c99cef7bcc614bbdad8c935de3639fc59
alaguthiyagarajan <132125380+alaguthiyagarajan@users.noreply.github.com>
2023-09-21 22:18:54 +0530

    db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mongodb = require("mongodb")
-const MongoClient=mongodb.MongoClient;
-const ObjectId=mongodb.ObjectId;
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
 
 //Database Connection
 const url = "mongodb+srv://alaguthiyagarajan:alaguthiyagarajan@demo.azcfhdp.mongodb.net/?retryWrites=true&w=majority";
 
-async function getDatabase(){
+async function getDatabase() {
     let connection = await MongoClient.connect( url );
     let database = connection.db( "waitList" );
     if(!database){
